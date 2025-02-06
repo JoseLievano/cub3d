01a156bf70cf44852a9407ceb787fb4220aefdcd
Vanesa Flores Moya <vflores-@c3r2s5.42luxembourg.lu>
2025-02-06 17:53:27 +0100

    parse

--- libs/parse/ft_parse.h
+++ libs/parse/ft_parse.h
@@ -1,9 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vflores- <vflores-@student.42luxembou      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 14:42:06 by vflores-          #+#    #+#             */
+/*   Updated: 2025/02/06 17:34:38 by vflores-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PARSE_H
 # define FT_PARSE_H
 # include "../../includes/cub3d.h"
 
+
+/*   MACROS   */
+
+# ifndef O_DIRECTORY
+#  define O_DIRECTORY 00200000
+# endif
+
+// ERROR MESSAGES
+# define ERR_FILE_IS_DIR "Is a directory"
+# define ERR_FILE_NOT_CUB "Not a .cub file"
+# define ERR_FILE_NOT_XPM "Not a .xpm file"
+# define ERR_INV_CHAR "Invalid character in map"
+# define ERR_NUM_PLAYER "Map has more than one player"
+# define ERR_PLAYER_DIR "Map has no player position (expected N, S, E or W)
+# define ERR_PLAYER_POS "Invalid player position"
+
+enum e_output
+{
+	FAILURE = 1;
+	SUCCESS = 0;
+}
+
 // For storing parsed textures and colors
-typedef struct s_textures {
+typedef struct s_textures
+{
 	char	*no_path;   // North texture path
 	char	*so_path;   // South texture path
 	char	*we_path;   // West texture path
@@ -13,15 +48,54 @@ typedef struct s_textures {
 } t_textures;
 
 // For storing map data and player spawn
-typedef struct s_map {
+typedef struct s_map
+{
+
+	//Datos generales del archivo
+	char	*path; //Ruta del archivo .cub
+	char	**file; //Contenido del archivo .cub linea por linea
+	int	line_count; //Numero total de lineas en el archivo	
+	
+	//Informacion del mapa
 	char		**grid;      // 2D map grid (from .cub)
 	int			rows;        // Map row count
 	int			cols;        // Map column count
-	int			spawn_x;     // Player starting X position
-	int			spawn_y;     // Player starting Y position
-	char		spawn_dir;   // Initial direction (N/S/E/W)
-	t_textures	*textures;  // Texture paths and colors
+	int		index_end_of_map; //indica el final del mapa
+	//Validacion del archivo
+	int	is_valid; //Flag que indica si el archivo esta
+			  //correctamente configurado
+
+	//Tecturas y colores
+	t_textures	textures;  // Texture paths and colors
 } t_map;
+
+typedef struct s_player
+{
+	double	pos_x; //Posocion actual del jugador en el mapa
+	double	pos_y; //Posicion actual del jugador en el mapa
+	char 	dir;
+	double	spawn_x; //Player starting X position
+	double	spawn_y; //Player starting Y position
+	char	spawn_dir; //Initial direction (N/S/E/W)
+}	t_player;
+
+typedef struct s_data
+{
+	t_player	player;
+	t_map	mapinfo;
+	char	**map;
+}
+
+typedef struct s_minimap
+{
+	char	**map;
+}
+//parse_args.c
+int	check_file(const char *args, bool cub);
+//ft_parse_utils.c
+size_t	get_max_line_len(t_map *map, int i);
+int	is_whitespace(char c);
+//map_parser.c
 
 void	ft_parse(void);
 void	ft_parse_test(int argc, char **argv);
---
+++ libs/parse/map_parser.c
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vflores- <vflores-@student.42luxembou      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 17:45:03 by vflores-          #+#    #+#             */
+/*   Updated: 2025/02/06 17:37:33 by vflores-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_parse.h"
+
+/* Verifica que el contenido de un mapa cumpla con los requisitos basicos:
+ * 	- Ignorar espacios en blanco
+ * 	- Validacion de caracteres
+ * 	- Validacion de la posicion inicial del jugador
+ * 	- Errores
+ */
+static int	validate_map(t_data *game_data, char **map_lines)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	game_data->player.dir = '\0';
+	while (map_lines[i] != NULL)
+	{
+		j = 0;
+		while (map_lines[i][j] != '\0')
+		{
+			while (ft_isspace(map_lines[i][j]))
+				j++;
+			if (!ft_strchr("10NSEW", map_lines[i][j]))
+				return (error_msg(game_data->mapinfo.path, ERR_INV_CHAR, FAILURE));
+			if (ft_strchr("NSEW", map_lines[i][j]))
+			{
+				if (game_data->player.dir != '\0')
+					return (error_msg(game_data->mapainfo.path, ERR_NUM_PLAYER, FAILURE));
+				game_data->playr.dir = map_lines[i][j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
+
+/* Verifica si la posicion inicial del jugador es valida en el mapa.
+ * El objetivo es asegurar que el jugador no este adyacente a un espacio
+ * vario y que las posiciones vecinas sean seguras.
+ */
+static int	check_pos(t_data *game_data, char **map_grid)
+{
+	int	i;
+	int	j;
+
+	i = (int)game_data->player.pos_y;
+	j = (int)game_data->player.pos_x;
+
+	if ((ft_strlen(map_grid[i - 1]) <= (size_t)j) ||
+	    (ft_strlen(map_grid[i + 1] <= (size_t)j) ||
+	    (is_whitespace(map_grid[i][j - 1]) == SUCCESS) ||
+	    (is_whitespace(map_grid[i][j + 1]) == SUCCESS) ||
+	    (is_whitespace(map_grid[i - 1][j]) == SUCCESS) ||
+	    (is_whitespace(map_grid[i + 1][j]) == SUCCESS))
+	    	return (FAILURE);
+	return (SUCCESS);
+}
+
+/* La funcion verifica que:
+ * 1. El jugador tenga una direccion inicial configurada
+ * 2. Encuentra la posicion inicial del jugador en el mapa
+ * 3. Guarda esa posicion en la estructura player
+ * 4. Sustituye la direccion en el mapa con un '0' para marcar la celda como transitable
+ * 5. Llama a una funcion auxiliar para verificar si la posicion inicial es valida.
+ */
+static int	check_player_pos(t_data *game, char **map_grid)
+{
+	int	i;
+	int	j;
+
+	if (game->player.dir == '0')
+		return (error_msg(game->mapinfo.path, ERR_PLAYER_DIR, FAILURE));
+	i = 0;
+	while (map_grid[i])
+	{
+		j = 0;
+		while (map_grid[i][j])
+		{
+			if (ft_strchr("NSEW", map_grid[i][j]))
+			{
+				game->player.pos_x = (double)j + 0.5;
+				game->player.pos_y = (double)i + 0.5;
+				map_grid[i][j] = '0';
+			}
+			j++;
+		}
+		i++;
+	}
+	if (check_pos(game, map_grid) == FAILURE)
+		return (error_msg(game->mapinfo.path, ERR_PLAYER_POS, FAILURE));
+	return (SUCCESS);
+}
+
+/* La funcion verifica que despues del final del mapa en el archivo .cub:
+ * 1. No haya contenido no permitido
+ * 2. Solo existan espacios en blanco en el resto del archivo
+ */
+static int	map_ends_correct(t_map *map_info)
+{
+	int	i;
+	int	j;
+
+	i = map_info->index_end_of_map;
+	while (map_info->file[i])
+	{
+		j = 0;
+		while (map_info->file[i][j])
+		{
+			if (!ft_strchr(" \t\r\n\v\f", map_info->file[i][j]))
+				return (FAILURE);
+			j++;
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	validate_map(t_data *game_data, char **map_content)
+{
+	if (!game_data->map)
+		return (error_msg(game_data->mapinfo.path, ERR_MAP_MISSING, FAILURE));
+	if (map
+}

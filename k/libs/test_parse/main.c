34f5b557f0da035915f02bdb9f730ec6b9e66ee5
JoseLievano <lievanosan@gmail.com>
2025-04-15 07:24:46 +0200

    cleaning parser

--- k/libs/test_parse/main.c
+++ k/libs/test_parse/main.c
@@ -7,9 +7,6 @@ int main(int argc, char **argv)
 		printf("Uso: %s mapa_test.cub\n", argv[0]);
 		return 1;
 	}
-	t_data game_data;
-	parse_data(argv[1], &game_data);
-
 	printf("Mapa cargado correctamente.\n");
 	return 0;
 }
